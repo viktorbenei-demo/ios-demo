ba97f1c6a8698ab5bb0d32f0c42801b0deff5612
Viktor Benei <viktor.benei@gmail.com>
2019-03-27 10:17:07 +0100

    init - base xcode project, right after generating it

---
+++ iOSDemoPotatotips/iOSDemoPotatotips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSDemoPotatotips
+//
+//  Created by Viktor Benei on 2019. 03. 27..
+//  Copyright © 2019. bitrise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
